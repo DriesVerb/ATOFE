4cd9e6b2f834b8fb1d9e2a31d92e6c574d099c57
DriesVerb <verboven.dries@gmail.com>
2020-12-29 21:44:40 +0100

    random emoji works for linux, firefox, chrome

---
+++ script.js
@@ -0,0 +1,19 @@
+let api = {
+  key: `d487b13a4a2bcb494ab0b506bff10656384c6df6`,
+  base: `https://emoji-api.com/emojis?access_key=`,
+};
+
+let emojiCollection = document.querySelectorAll(
+  `.emoji__generator .div__emoji`
+);
+
+fetch(`https://emoji-api.com/emojis?access_key=${api.key}`)
+  .then((emoji) => emoji.json())
+
+  .then((object) => {
+    emojiCollection.forEach((value, index) => {
+      let randomizer = Math.floor(Math.random() * object.length);
+      // if (object[randomizer].character !== "🧋")
+      value.innerHTML = `${object[randomizer].character}`;
+    });
+  });
